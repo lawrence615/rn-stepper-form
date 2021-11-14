237b7a57f20ca4e71bfe9f4c0b5a52ee56fabd8e
Lawrence Macharia <macharialawrence2011@gmail.com>
2021-11-14 05:03:07 +0300

    code improvement

--- src/presentation/components/onboarding/Step04.js
+++ src/presentation/components/onboarding/Step04.js
@@ -1,9 +1,23 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect, useCallback } from 'react'
+import { SafeAreaView, View, Text, TextInput } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
+import tw from 'tailwind-react-native-classnames'
+import { useFocusEffect } from '@react-navigation/native';
+
+import { setStep } from 'reducers/authStepperSlice'
 
 const Step04 = () => {
+
+  const dispatch = useDispatch()
+
+  useFocusEffect(
+    useCallback(() => {
+      dispatch(setStep(4))
+    }, [])
+  );
+
   return (
-    <View>
+    <View style={tw`flex-1`}>
       <Text>Step04</Text>
     </View>
   )
