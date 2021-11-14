849f0af91a31e44c758f709e0072e69ea173397f
Lawrence Macharia <macharialawrence2011@gmail.com>
2021-11-14 05:10:14 +0300

    dynamically loading the steps now

--- src/presentation/components/onboarding/Header.js
+++ src/presentation/components/onboarding/Header.js
@@ -3,40 +3,33 @@ import { View, Text } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 import tw from 'tailwind-react-native-classnames'
 
-import { selectStep } from 'selectors/authStepperSlice'
+import { selectSteps, selectStep } from 'selectors/authStepperSlice'
 
 const Header = () => {
 
+  const steps = useSelector(selectSteps)
   const step = useSelector(selectStep)
 
+
+  const renderSteps = () => {
+    return steps.map((item, index) => {
+      console.log(item)
+      return (
+        <View key={index} style={tw`py-4 px-2 w-24 rounded-l-md ${step === index + 1 && "bg-green-100"}`}>
+          <Text style={tw`text-xs font-normal`}>{item.label}</Text>
+          <Text style={tw`text-sm font-medium`}>{item.desc}</Text>
+        </View>
+      )
+    })
+  }
 
   const onNextStep = (step) => {
 
   }
 
-  // useEffect(() => {
-  //   console.log('current step ' + step)
-  //   // dispatch(setStep(1))
-  // }, [step])
-
   return (
     <View style={tw`flex flex-row justify-between bg-gray-100 rounded-md`}>
-      <View style={tw`py-4 px-2 w-24 rounded-l-md ${step === 1 && "bg-green-100"}`}>
-        <Text style={tw`text-xs font-normal`}>STEP 01</Text>
-        <Text style={tw`text-sm font-medium`}>Start</Text>
-      </View>
-      <View style={tw`py-4 px-2 w-24 ${step === 2 && "bg-green-100"}`}>
-        <Text style={tw`text-xs font-normal`}>STEP 02</Text>
-        <Text style={tw`text-sm font-medium`}>Details</Text>
-      </View>
-      <View style={tw`py-4 px-2 w-24 ${step === 3 && "bg-green-100"}`}>
-        <Text style={tw`text-xs font-normal`}>STEP 03</Text>
-        <Text style={tw`text-sm font-medium`}>Topics</Text>
-      </View>
-      <View style={tw`py-4 px-2 w-24 rounded-r-md ${step === 4 && "bg-green-100"}`}>
-        <Text style={tw`text-xs font-normal`}> STEP 04</Text>
-        <Text style={tw`text-sm font-medium`}>Confirm</Text>
-      </View>
+      {renderSteps()}
     </View>
   )
 }
