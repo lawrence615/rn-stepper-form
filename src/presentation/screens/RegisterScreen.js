93fa5473b6f22d5611610078741615321de6d40d
Lawrence Macharia <macharialawrence2011@gmail.com>
2021-11-14 05:01:33 +0300

    simplified the view by having only navigation components

--- src/presentation/screens/RegisterScreen.js
+++ src/presentation/screens/RegisterScreen.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import tw from 'tailwind-react-native-classnames'
+
+import PickRole from 'components/onboarding/PickRole'
+import Main from 'components/onboarding/Main'
 
 const RegisterScreen = () => {
+
+  const Stack = createNativeStackNavigator()
+
   return (
-    <View>
-      <Text> Register Screen </Text>
+    <View style={tw`bg-white flex-1 p-5`}>
+      <Stack.Navigator initialRouteName="PickRole">
+        <Stack.Screen name="PickRole" component={PickRole} options={{ headerShown: false }} />
+        <Stack.Screen name="StartStepper" component={Main} options={{ headerShown: false }} />
+      </Stack.Navigator>
     </View>
   )
 }
