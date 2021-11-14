2a370e15993f01a3b511330f95c35a697109fc27
Lawrence Macharia <macharialawrence2011@gmail.com>
2021-11-14 05:05:52 +0300

    removed unused imports

--- src/presentation/components/onboarding/Step03.js
+++ src/presentation/components/onboarding/Step03.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useCallback } from 'react'
+import { View, TextInput } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
+import tw from 'tailwind-react-native-classnames'
+import { useFocusEffect } from '@react-navigation/native';
+
+import { setStep } from 'reducers/authStepperSlice'
 
 const Step03 = () => {
+
+  const dispatch = useDispatch()
+
+  useFocusEffect(
+    useCallback(() => {
+      dispatch(setStep(3))
+    }, [])
+  );
+
   return (
-    <View>
-      <Text>Step03</Text>
+    <View style={tw`flex flex-1 flex-col py-10`}>
+      <TextInput
+        style={tw`bg-gray-200 p-4 rounded-md`}
+        placeholder="Password"
+        textContentType="newPassword" />
+      <TextInput
+        style={tw`bg-gray-200 p-4 mt-10 mb-10 rounded-md`}
+        placeholder="Confirm password"
+        textContentType="newPassword" />
     </View>
   )
 }
