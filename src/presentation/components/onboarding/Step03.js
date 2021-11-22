38ccd89b23b839807f1cb12d7d726de1f3fcdf6f
Lawrence Macharia <macharialawrence2011@gmail.com>
2021-11-22 09:15:28 +0300

    added the same password effects on the Step03 screen

--- src/presentation/components/onboarding/Step03.js
+++ src/presentation/components/onboarding/Step03.js
@@ -1,6 +1,7 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { View, TextInput } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
+import { Icon } from 'react-native-elements'
 import tw from 'tailwind-react-native-classnames'
 import { useFocusEffect } from '@react-navigation/native';
 
@@ -8,7 +9,29 @@ import { setStep } from 'reducers/authStepperSlice'
 
 const Step03 = () => {
 
+  const [hideNewPassword, setHideNewPassword] = useState(true)
+  const [hideConfirmNewPassword, setHideConfirmNewPassword] = useState(true)
   const dispatch = useDispatch()
+
+  const renderNewPasswordIcon = () => {
+    return hideNewPassword
+      ? <Icon style={tw`right-0`} name='eye-off-outline' type='ionicon' onPress={onNewPasswordShow} />
+      : <Icon style={tw`right-0`} name='eye-outline' type='ionicon' onPress={onNewPasswordShow} />
+  }
+
+  const renderConfirmNewPasswordIcon = () => {
+    return hideConfirmNewPassword
+      ? <Icon style={tw`right-0`} name='eye-off-outline' type='ionicon' onPress={onConfirmNewPasswordShow} />
+      : <Icon style={tw`right-0`} name='eye-outline' type='ionicon' onPress={onConfirmNewPasswordShow} />
+  }
+
+  const onNewPasswordShow = () => {
+    return hideNewPassword ? setHideNewPassword(false) : setHideNewPassword(true)
+  }
+
+  const onConfirmNewPasswordShow = () => {
+    return hideConfirmNewPassword ? setHideConfirmNewPassword(false) : setHideConfirmNewPassword(true)
+  }
 
   useFocusEffect(
     useCallback(() => {
@@ -18,16 +41,22 @@ const Step03 = () => {
 
   return (
     <View style={tw`flex flex-1 flex-col py-10`}>
-      <TextInput
-        style={tw`bg-gray-200 p-4 rounded-md`}
-        placeholder="Password"
-        secureTextEntry={true}
-        textContentType="newPassword" />
-      <TextInput
-        style={tw`bg-gray-200 p-4 mt-10 mb-10 rounded-md`}
-        placeholder="Confirm password"
-        secureTextEntry={true}
-        textContentType="newPassword" />
+      <View style={tw`flex-row items-center bg-gray-200 p-2 rounded-md`}>
+        <TextInput
+          style={tw`flex-1`}
+          placeholder="Password"
+          textContentType="newPassword"
+          secureTextEntry={hideNewPassword} />
+        {renderNewPasswordIcon()}
+      </View>
+      <View style={tw`flex-row items-center bg-gray-200 p-2 mt-10 mb-10 rounded-md`}>
+        <TextInput
+          style={tw`flex-1`}
+          placeholder="Confirm password"
+          textContentType="newPassword"
+          secureTextEntry={hideConfirmNewPassword} />
+        {renderConfirmNewPasswordIcon()}
+      </View>
     </View>
   )
 }
